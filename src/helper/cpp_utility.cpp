f4c01c1d558929a97a90995f9a2c445abb688837
hbb_mac <kok.bing@qq.com>
2024-07-13 15:50:08 +0800

    code style -

--- src/helper/cpp_utility.cpp
+++ src/helper/cpp_utility.cpp
@@ -1,87 +1,83 @@
 #include "include/prism/qt/ui/helper/cpp_utility.h"
-#include "src/controls/window/qml_debug_window.h"
 #include "../platform/i_borderless_window_helper.h"
-#include <prism/container.hpp>
+#include "src/controls/window/qml_debug_window.h"
 #include <QApplication>
 #include <QCoreApplication>
 #include <QDebug>
+#include <QDesktopServices>
+#include <QFileInfo>
 #include <QQmlApplicationEngine>
 #include <QQuickWindow>
 #include <QTranslator>
-#include <QFileInfo>
-#include <QDesktopServices>
+#include <prism/container.hpp>
 
 #include <QDebug>
 
 #ifdef USING_PCL
-    #include <QQuickVTKRenderWindow.h>
-    #include <QQuickWindow>
-    #include <QSGRendererInterface>
-    #include <QSurfaceFormat>
+#include <QQuickVTKRenderWindow.h>
+#include <QQuickWindow>
+#include <QSGRendererInterface>
+#include <QSurfaceFormat>
 #endif
 
-namespace prism::qt::ui {
+namespace prism::qt::ui
+{
 cpp_utility::cpp_utility(QObject* parent)
     : QObject(parent)
 {
-    //detect ui hang timer
+    // detect ui hang timer
     uiTimer_ = std::make_unique<QTimer>();
     setCppOwnership(uiTimer_.get());
-    connect(uiTimer_.get(),&QTimer::timeout,this,[this](){
+    connect(uiTimer_.get(), &QTimer::timeout, this, [this]() {
         pre_timepoint_ = QDateTime::currentMSecsSinceEpoch();
     });
 
-
-    //detect qml live
+    // detect qml live
     int length = QCoreApplication::arguments().length();
-    for(int i = 0; i < length; ++i)
+    for (int i = 0; i < length; ++i)
     {
         QString arg = QCoreApplication::arguments().at(i);
 
-        if(arg == "qml_live")
+        if (arg == "qml_live")
         {
             isqmllive_ = true;
         }
     }
 
-
     uiTimer_->setSingleShot(false);
     uiTimer_->setInterval(uiTimer_interval_);
     uiTimer_->start();
-
 }
 
 cpp_utility::~cpp_utility()
 {
-    if(uiTimer_)
+    if (uiTimer_)
         uiTimer_->stop();
 }
 
 bool cpp_utility::is_ui_hang()
 {
     auto delta = QDateTime::currentMSecsSinceEpoch() - pre_timepoint_;
-    if(delta > uiTimer_interval_*10)
+    if (delta > uiTimer_interval_ * 10)
         return true;
     else
         return false;
 }
 
-void cpp_utility::setQmlOwnership(QObject *obj)
+void cpp_utility::setQmlOwnership(QObject* obj)
 {
-    if(QQmlEngine::objectOwnership(obj) != QQmlEngine::JavaScriptOwnership)
+    if (QQmlEngine::objectOwnership(obj) != QQmlEngine::JavaScriptOwnership)
     {
-        QQmlEngine::setObjectOwnership(obj,QQmlEngine::ObjectOwnership::JavaScriptOwnership);
+        QQmlEngine::setObjectOwnership(obj, QQmlEngine::ObjectOwnership::JavaScriptOwnership);
     }
-
 }
 
-void cpp_utility::setCppOwnership(QObject *obj)
+void cpp_utility::setCppOwnership(QObject* obj)
 {
-    if(QQmlEngine::objectOwnership(obj) != QQmlEngine::CppOwnership)
+    if (QQmlEngine::objectOwnership(obj) != QQmlEngine::CppOwnership)
     {
-        QQmlEngine::setObjectOwnership(obj,QQmlEngine::ObjectOwnership::CppOwnership);
+        QQmlEngine::setObjectOwnership(obj, QQmlEngine::ObjectOwnership::CppOwnership);
     }
-
 }
 
 void cpp_utility::enableWindowBorderless(QQuickWindow* win)
@@ -187,13 +183,13 @@ double cpp_utility::itof(int i)
 
 QPoint cpp_utility::getMousePos()
 {
-    return  QCursor::pos();
+    return QCursor::pos();
 }
 
-void cpp_utility::forceUpdateMouseAreaHovered(QObject *mouseArea)
+void cpp_utility::forceUpdateMouseAreaHovered(QObject* mouseArea)
 {
-    //QEvent* e = new QEvent(QEvent::MouseMove);
-    //QCoreApplication::sendEvent(mouseArea, e);
+    // QEvent* e = new QEvent(QEvent::MouseMove);
+    // QCoreApplication::sendEvent(mouseArea, e);
 
     //{
     //    QMouseEvent *enterEvent = new QMouseEvent(QEvent::Enter, QPointF(10, 10), Qt::NoButton, Qt::NoButton, Qt::NoModifier);
@@ -206,7 +202,7 @@ void cpp_utility::forceUpdateMouseAreaHovered(QObject *mouseArea)
     //}
 
     {
-        QMouseEvent *exitEvent = new QMouseEvent(QEvent::MouseMove, QPointF(-1, -1), Qt::NoButton, Qt::NoButton, Qt::NoModifier);
+        QMouseEvent* exitEvent = new QMouseEvent(QEvent::MouseMove, QPointF(-1, -1), Qt::NoButton, Qt::NoButton, Qt::NoModifier);
         QCoreApplication::sendEvent(mouseArea, exitEvent);
     }
 }
@@ -216,10 +212,9 @@ QModelIndex cpp_utility::getInvalidQModelIndex()
     return QModelIndex();
 }
 
-
-std::shared_ptr<bool> cpp_utility::showQmlDialog(QString qmlUrl,QObject* viewModel)
+std::shared_ptr<bool> cpp_utility::showQmlDialog(QString qmlUrl, QObject* viewModel)
 {
-    std::shared_ptr<bool> result ;
+    std::shared_ptr<bool> result;
     std::shared_ptr<QQmlApplicationEngine> p_engine = Container::get()->resolve_object<QQmlApplicationEngine>();
     QQmlComponent component(p_engine.get());
 
@@ -227,13 +222,13 @@ std::shared_ptr<bool> cpp_utility::showQmlDialog(QString qmlUrl,QObject* viewMod
     auto url = transUrl("qrc:/prism_qt_ui/window/QmlDebugWindow.qml");
     component.loadUrl(url);
     qmlDebugWindow* win = nullptr;
-    if( component.isReady())
+    if (component.isReady())
     {
         QEventLoop loop;
-        auto a =   component.create();
-        win =  reinterpret_cast<qmlDebugWindow*>(a);
+        auto a = component.create();
+        win = reinterpret_cast<qmlDebugWindow*>(a);
         win->setViewModel(QVariant::fromValue(viewModel));
-        QObject::connect(win,&QQuickWindow::destroyed,&loop,[&](auto* p){
+        QObject::connect(win, &QQuickWindow::destroyed, &loop, [&](auto* p) {
             Q_UNUSED(p)
             loop.exit();
         });
@@ -242,15 +237,15 @@ std::shared_ptr<bool> cpp_utility::showQmlDialog(QString qmlUrl,QObject* viewMod
         loop.exec();
 
         //#undef Bool
-        //if(win->dialogResult().canConvert(QVariant::Bool))
+        // if(win->dialogResult().canConvert(QVariant::Bool))
         //{
         //    result = std::make_shared<bool>(win->dialogResult().value<bool>());
         //}
 
-        //loop.disconnect(win);
+        // loop.disconnect(win);
 
-        //if(win)
-        //    win->deleteLater();
+        // if(win)
+        //     win->deleteLater();
     }
     else
     {
@@ -259,86 +254,82 @@ std::shared_ptr<bool> cpp_utility::showQmlDialog(QString qmlUrl,QObject* viewMod
     }
 
     return result;
-
 }
 
-void cpp_utility::showQmlWindow(QString qmlUrl,QObject* viewModel)
+void cpp_utility::showQmlWindow(QString qmlUrl, QObject* viewModel)
 {
     QQuickWindow::setDefaultAlphaBuffer(true);
 
     std::shared_ptr<QQmlApplicationEngine> p_engine = Container::get()->resolve_object<QQmlApplicationEngine>();
     std::shared_ptr<QQuickWindow> mainWindow = prism::Container::get()->resolve_object<QQuickWindow>();
-    QQmlComponent component(p_engine.get(),mainWindow->contentItem());
+    QQmlComponent component(p_engine.get(), mainWindow->contentItem());
 
     auto url = transUrl("qrc:/prism_qt_ui/window/QmlDebugWindow.qml");
     component.loadUrl(url);
-    if ( component.isReady() )
+    if (component.isReady())
     {
-        auto a =   component.create();
-        qmlDebugWindow* win =  reinterpret_cast<qmlDebugWindow*>(a);
+        auto a = component.create();
+        qmlDebugWindow* win = reinterpret_cast<qmlDebugWindow*>(a);
         win->setViewModel(QVariant::fromValue(viewModel));
 
         win->setLoadUrl(qmlUrl);
     }
     else
         qWarning() << component.errorString();
-
 }
 
 bool cpp_utility::load_language_qm(QString filePath)
 {
     try
     {
-        std::shared_ptr<QTranslator> translator =  prism::Container::get()->resolve_object<QTranslator>();
-        if(!translator)
+        std::shared_ptr<QTranslator> translator = prism::Container::get()->resolve_object<QTranslator>();
+        if (!translator)
         {
-            if(!QCoreApplication::removeTranslator(translator.get()))
+            if (!QCoreApplication::removeTranslator(translator.get()))
             {
-                std::shared_ptr<QTranslator> empty ;
+                std::shared_ptr<QTranslator> empty;
                 prism::Container::get()->register_instance(empty);
             }
             else
                 throw std::string("卸载多语言翻译器失败");
         }
 
-        if(!filePath.isEmpty())
+        if (!filePath.isEmpty())
         {
             std::shared_ptr<QTranslator> tr = std::make_shared<QTranslator>();
-            if(tr->load(filePath))
+            if (tr->load(filePath))
             {
-                if(QCoreApplication::installTranslator(tr.get()))
+                if (QCoreApplication::installTranslator(tr.get()))
                     prism::Container::get()->register_instance(tr);
                 else
                     throw std::string("安装多语言翻译器失败");
-
             }
             else
                 throw std::string("加载多语言文件失败:") + filePath.toStdString();
-
         }
         else
         {
-            qInfo()<<"正在卸载非中文的多语言翻译器";
+            qInfo() << "正在卸载非中文的多语言翻译器";
         }
         //通知qml engine，如果有的话
         std::shared_ptr<QQmlApplicationEngine> engine = Container::get()->resolve_object<QQmlApplicationEngine>();
-        if(engine)
+        if (engine)
         {
             /**
              * fix 1 - retranslate will refresh all qml property,cause qsortfilterproxymodel* get an freed address which is incorret
              * fix 2 - Performance when switching languages ​​dynamically
              * by hbb 2024.2.3
              */
-            //engine->retranslate();
-            if(transThis()=="")
-                setTransThis("\u200B");//ZWSP
+            // engine->retranslate();
+            if (transThis() == "")
+                setTransThis("\u200B"); // ZWSP
             else
                 setTransThis("");
         }
     }
-    catch(std::string & mes)
+    catch (std::string& mes)
     {
-        qCritical()<< QString::fromStdString(mes);
+        qCritical() << QString::fromStdString(mes);
         return false;
     }
     return true;
@@ -359,38 +350,41 @@ void cpp_utility::openPath(const QString path)
 {
     QFileInfo fileInfo(path);
 
-    if (fileInfo.isDir()) {
+    if (fileInfo.isDir())
+    {
         // 如果是目录，直接打开目录
         QDesktopServices::openUrl(QUrl::fromLocalFile(path));
-    } else if (fileInfo.isFile()) {
+    }
+    else if (fileInfo.isFile())
+    {
         // 如果是文件，打开文件所在的目录，并定位到文件
         QDesktopServices::openUrl(QUrl::fromLocalFile(fileInfo.absolutePath()));
-    } else {
+    }
+    else
+    {
         // 路径既不是目录也不是文件，显示错误信息
-        qDebug()<< "open invalidate path :" << path;
+        qDebug() << "open invalidate path :" << path;
     }
 }
 
-QObject *cpp_utility::getModelIndexInternalPointer(const QModelIndex &item)
+QObject* cpp_utility::getModelIndexInternalPointer(const QModelIndex& item)
 {
     return static_cast<QObject*>(item.internalPointer());
 }
-
 
 bool cpp_utility::isqmllive()
 {
     return isqmllive_;
 }
 
+std::string cpp_utility::openGLVersion = "default";
 
-std::string cpp_utility::openGLVersion  = "default";
-
-const QString &cpp_utility::transThis() const
+const QString& cpp_utility::transThis() const
 {
     return m_transThis;
 }
 
-void cpp_utility::setTransThis(const QString &newTransThis)
+void cpp_utility::setTransThis(const QString& newTransThis)
 {
     if (m_transThis == newTransThis)
         return;
@@ -398,13 +392,13 @@ void cpp_utility::setTransThis(const QString &newTransThis)
     emit transThisChanged();
 }
 
-}// namespace prism::qt::ui
+} // namespace prism::qt::ui
 
 #ifdef USING_PCL
 void initVTK()
 {
     qDebug() << "set openGL version " << QString::fromStdString(prism::qt::ui::cpp_utility::openGLVersion);
-    if(prism::qt::ui::cpp_utility::openGLVersion =="3.2")
+    if (prism::qt::ui::cpp_utility::openGLVersion == "3.2")
     {
         QSurfaceFormat fmt;
         fmt.setRenderableType(QSurfaceFormat::OpenGL);
@@ -427,11 +421,11 @@ void initVTK()
         //    QQuickWindow::setGraphicsApi(QSGRendererInterface::OpenGLRhi);
         //#endif
     }
-    else if(prism::qt::ui::cpp_utility::openGLVersion == "3.0")
+    else if (prism::qt::ui::cpp_utility::openGLVersion == "3.0")
     {
         std::string mesa_gl_version = std::string("MESA_GL_VERSION_OVERRIDE=") + prism::qt::ui::cpp_utility::openGLVersion;
 
-        qDebug()<< QString::fromStdString(mesa_gl_version);
+        qDebug() << QString::fromStdString(mesa_gl_version);
 
         char* env = (char*)mesa_gl_version.c_str();
 #ifdef __linux
@@ -441,11 +435,9 @@ void initVTK()
 #endif
         DV_PUTENV(env);
 
-        DV_PUTENV((char* )"MESA_GL_VERSION_OVERRIDE=3.0");
+        DV_PUTENV((char*)"MESA_GL_VERSION_OVERRIDE=3.0");
         // Fixes decimal point issue in vtkSTLReader
-        DV_PUTENV((char *)"LC_NUMERIC=C");
-
+        DV_PUTENV((char*)"LC_NUMERIC=C");
     }
-
 }
 #endif
