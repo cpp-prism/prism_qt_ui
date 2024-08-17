96347fc6c4d527d51b47a5601c1f4ddd7c6216ee
hbb_mac <kok.bing@qq.com>
2024-08-17 17:06:24 +0800

    feat - add open qwidget color dialog from qml

--- include/prism/qt/ui/helper/cpp_utility.h
+++ include/prism/qt/ui/helper/cpp_utility.h
@@ -2,19 +2,22 @@
 #define PRISM_QT_UI_CPP_UTILITY_H
 
 #include "../prismQt_ui_global.h"
-#include <QObject>
-#include <QUrl>
-#include <QVariant>
-#include <memory>
-#include <QPoint>
-#include <QTimer>
 #include <QDateTime>
 #include <QModelIndex>
+#include <QObject>
+#include <QPoint>
+#include <QTimer>
+#include <QUrl>
+#include <QUuid>
+#include <QVariant>
+#include <QtWidgets/QColorDialog>
+#include <QtWidgets/QDialog>
+#include <memory>
 #include <prism/utilities/typeName.hpp>
 
-
 class QQuickWindow;
-namespace prism::qt::ui {
+namespace prism::qt::ui
+{
 
 class PRISMQT_UI_EXPORT cpp_utility : public QObject
 {
@@ -23,14 +26,14 @@ class PRISMQT_UI_EXPORT cpp_utility : public QObject
   public:
     explicit cpp_utility(QObject* parent = nullptr);
     ~cpp_utility();
-    static std::string openGLVersion ;
+    static std::string openGLVersion;
     bool is_ui_hang();
-    const QString &transThis() const;
-    void setTransThis(const QString &newTransThis);
+    const QString& transThis() const;
+    void setTransThis(const QString& newTransThis);
 
-public slots:
-    void setCppOwnership(QObject *obj);
-    void setQmlOwnership(QObject *obj);
+  public slots:
+    void setCppOwnership(QObject* obj);
+    void setQmlOwnership(QObject* obj);
     void enableWindowBorderless(QQuickWindow* win);
     void clearQmlCache();
     bool enableHotReload();
@@ -45,8 +48,8 @@ public slots:
 
     QModelIndex getInvalidQModelIndex();
 
-    std::shared_ptr<bool> showQmlDialog(QString qmlUrl,QObject* viewModel);
-    void showQmlWindow(QString qmlUrl, QObject* viewModel =nullptr);
+    std::shared_ptr<bool> showQmlDialog(QString qmlUrl, QObject* viewModel);
+    void showQmlWindow(QString qmlUrl, QObject* viewModel = nullptr);
 
     bool load_language_qm(QString filePath);
 
@@ -55,28 +58,31 @@ public slots:
     bool isqmllive();
     void openPath(const QString path);
     QObject* getModelIndexInternalPointer(const QModelIndex& item);
+    QString getguid();
 
-private:
-    std::unique_ptr<QTimer> uiTimer_ ;
-    int uiTimer_interval_ = 50 ;
-    std::time_t pre_timepoint_ ;
+    QColorDialog* openColorWindow(QWidget* parent);
+    int getDialogResult(QDialog* d);
+    QString qcolor2qstring(QColor color);
+
+  private:
+    std::unique_ptr<QTimer> uiTimer_;
+    int uiTimer_interval_ = 50;
+    std::time_t pre_timepoint_;
     bool isqmllive_ = false;
 
     QString m_transThis;
 
-signals:
+  signals:
     void transThisChanged();
 };
 
-}// namespace prism::qt::ui
-
+} // namespace prism::qt::ui
 
 #ifdef USING_PCL
 extern "C"
 {
-PRISMQT_UI_EXPORT void initVTK();
+    PRISMQT_UI_EXPORT void initVTK();
 }
 #endif
-
 
 #endif // PRISM_QT_UI_CPP_UTILITY_H
