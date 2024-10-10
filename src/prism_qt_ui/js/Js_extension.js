304976e1010f03eb8b070686c2710f63ac9b0b94
root <huangbinbin@deepvision-tech.com>
2024-10-10 12:26:50 +0000

    feat - js utility add get currentdatetime yyyy-MM-DD HH:mm:ss.zzz

--- src/prism_qt_ui/js/Js_extension.js
+++ src/prism_qt_ui/js/Js_extension.js
@@ -22,6 +22,18 @@ function currentDateTimeString() {
 
     return qsTr(year + "-" + month + "-" + day + " " + hours + ":" + minutes + ":" + seconds);
 }
+function currentDateTimeZZZString() {
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = ("0" + (date.getMonth() + 1)).slice(-2);
+    var day = ("0" + date.getDate()).slice(-2);
+    var hours = ("0" + date.getHours()).slice(-2);
+    var minutes = ("0" + date.getMinutes()).slice(-2);
+    var seconds = ("0" + date.getSeconds()).slice(-2);
+    var msecodns = ("0" + date.getMilliseconds()).slice(-3);
+
+    return qsTr(year + "-" + month + "-" + day + " " + hours + ":" + minutes + ":" + seconds+"."+msecodns);
+}
 
 function prismTimer(parent) {
     return Qt.createQmlObject("import QtQuick 2.0; Timer {}", parent)
